eaf2f11b9cb792d5f885450f385f90759aa5d944
Payton Harris <paytonharris000@gmail.com>
2024-01-08 11:05:18 -0500

    Add neovim folder contents

---
+++ .config/nvim/lua/custom/mappings.lua
@@ -0,0 +1,47 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    -- [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
+    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
+    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
+    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+
+  },
+  v = {
+    [">"] = { ">gv", "indent"},
+  },
+}
+
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = { "<cmd> DapToggleBreakpoint <CR>" },
+    ["<leader>dus"] = {
+      function ()
+        local widgets = require('dap.ui.widgets');
+        local sidebar = widgets.sidebar(widgets.scopes);
+        sidebar.open();
+      end,
+      "Open debugging sidebar"
+    }
+  }
+}
+
+M.crates = {
+  plugin = true,
+  n = {
+    ["<leader>rcu"] = {
+      function ()
+        require('crates').upgrade_all_crates()
+      end,
+      "update crates"
+    }
+  }
+}
+
+-- more keybinds!
+
+return M
